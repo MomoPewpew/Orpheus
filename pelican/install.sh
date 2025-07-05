5a041c66215bb90bbba43bb533c4c196fb0ce16f
MomoPewpew <marijntepas@gmail.com>
2025-07-05 17:29:05 +0200

    Update Orpheus version to 1.2.2 in installation scripts and configuration for improved functionality and compatibility.

--- pelican/install.sh
+++ pelican/install.sh
@@ -5,7 +5,7 @@ echo "Installing Orpheus..."
 
 # Download and extract Orpheus
 echo "Downloading Orpheus..."
-curl -L https://github.com/MomoPewpew/Orpheus/releases/download/v1.2.1/Orpheus.v1.2.1-pelican.zip -o orpheus.zip
+curl -L https://github.com/MomoPewpew/Orpheus/releases/download/v1.2.2/Orpheus.v1.2.2-pelican.zip -o orpheus.zip
 unzip orpheus.zip
 rm orpheus.zip
 
